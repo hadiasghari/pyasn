8e84bdf8eb0cd8002f0866bdb2f4f47c8c36cc98
Hadi Asghari <hadiasghari@users.noreply.github.com>
2014-05-27 21:40:59 +0200

    updated how the as-names will be read (from a seperate file)

--- pyasn-helper/pyasn_db_helper.py
+++ pyasn-helper/pyasn_db_helper.py
@@ -3,93 +3,63 @@
 # todo: perhaps some of the other python files would also be better combinet with this
 
 import socket, struct
-import pickle, zlib, base64
+import pickle, zlib
+import time
 
-
-def convert_ipasndb_to_binary(filename):
-    f = open(filename)
-    fw = open(filename+'.bin', 'wb')
+def convert_ipasndb_to_binary(ipasndb_file, asnames_file, binary_outfile):
+    fw = open(binary_outfile, 'wb')
     # SHOW STRUCTURE ADD HEADER-VER to binary file to show ipasn version + date
     fw.write(str.encode('PYASN'))  # magic header
     fw.write(b'\x01')  # binary format version 1
     fw.write(struct.pack('I', 0))  # number of records; will need to be updated at the end.
     nbytes = 12
     
-    # let's store comments at start of text file as is in the binary file, useful for debugging, etc
-    initial_comments = ""    
-    for s in f:
-        if s[0] != '#' and s[0] != '\n' and s[0] != ';':
-            break
-        initial_comments += s
-    initial_comments = initial_comments.encode('ASCII', errors='replace')[:499] + b'\0'  #convert to bytes, trim, null terminate
-    fw.write(struct.pack('h', len(initial_comments))) 
-    fw.write(initial_comments)
-    nbytes += len(initial_comments)
-    
-    # main loop - convert prefixes
-    f.seek(0)
-    nrecs = 0
-    for s in f:
-        if s.startswith('; START-AS-NAMES'):
-            break  # finished prefix part
-        if s[0] == '#' or s[0]=='\n' or s[0] == ';':
-            continue 
-        prefix, asn = s[:-1].split()     
-        asn = int(asn)
-        network, cidr = prefix.split('/')
-        #network = struct.unpack('>I', socket.inet_aton(network))[0]
-        cidr = int(cidr)
-        nbytes += fw.write(socket.inet_aton(network))  # STORE THE INET VERSION, not INT. C code reads it like this too
-        nbytes += fw.write(struct.pack('B', cidr))
-        nbytes += fw.write(struct.pack('I', asn))
-        nrecs += 1
-    #    
+    # let's store as comments the name of the input files used to build the binary file. good for debugging.
+    comments = "Created '%s', from ipasndb: %s, asnames: %s." % (time.actime(), ipasndb_file, asnames_file)
+    comments = comments.encode('ASCII', errors='replace')[:499] + b'\0'  # convert to bytes, trim, terminate
+    fw.write(struct.pack('h', len(comments)))
+    fw.write(comments)
+    nbytes += len(comments)
+
+    with open(ipasndb_file) as f:
+        nrecs = 0
+        for s in f:
+            if s[0] == '#' or s[0]=='\n' or s[0] == ';':
+                continue
+            prefix, asn = s[:-1].split()
+            asn = int(asn)
+            network, cidr = prefix.split('/')
+            cidr = int(cidr)
+            nbytes += fw.write(socket.inet_aton(network))
+            nbytes += fw.write(struct.pack('B', cidr))
+            nbytes += fw.write(struct.pack('I', asn))
+            nrecs += 1
+
     fw.write(bytes(9))  # write one zero record
-    blob = ""
-    # there might be as-names at the end; if so, convert them too. 
-    for s in f:
-        if s.startswith('; END-AS-NAMES'):
-            break       
-        assert s[0] == ';'
-        blob.append(s[1:-1]) 
-    if blob:
-        blob = base64.b64decode(blob)  
-        fw.write(struct.pack('I', len(blob)))  # number of bytes
-        fw.write(blob)
+    if asnames_file:
+        # todo: i'm not sure this is the best way to store this
+        d = load_asnames_dict(asnames_file)
+        z = zlib.compress(pickle.dumps(d))
+        fw.write(struct.pack('I', len(z)))  # number of bytes
+        fw.write(z)
     # almost done. update number of records at start of file.
-    f.close()        
     fw.seek(6)
     fw.write(struct.pack('I', nrecs))
     fw.close()    
     return nbytes, nrecs
              
 
-def convert_asnamelist_to_b64blob(filein, fileout):
-    """convert an asn + as-name list (one per line, space after ASN, 32bits with dots)
-    to the compressed-based64-asnames-dictionary (readable by pyasn)
-    """
-    # todo: compare this with using ANYDB regarding sizes, etc
-    # todo: perhaps it's better if the function returns a string, instead of writing directly to an out-file
-    # todo: Test on py2 (re utf/bytes). 
+def load_asnames_dict(filein):
+    # loads an as-names file into a dictionary
+    # current format: one per line, space after ASN, 32bits with dots
+    # todo: chose format of this file based on final decisions in pyasn.pyasn()
     asnames = {}
-    f = open(filein, encoding='utf-8') 
+    f = open(filein, encoding='utf-8')  # todo: Test on py2 (re utf/bytes).
     for s in f:
         asn, asname = s[:-1].split(maxsplit=1)
         if asname == '-Reserved AS-':
             continue
         asn = int(asn[2:]) if '.' not in asn else int(asn[2:asn.find('.')])*65536 +  int(asn[asn.find('.')+1:]) 
         asnames[asn] = asname
-    f.close()    
-        
-    s = pickle.dumps(asnames)
-    z = zlib.compress(s) 
-    zb = base64.b64encode(z)
-
-    fw = open(fileout, 'wb')
-    fw.write(b'; START-AS-NAMES\n')
-    for i in range(0, len(zb), 79):
-        fw.write(b';')
-        fw.write(zb[i:i+79])
-        fw.write(b'\n')
-    fw.write(b'; END-AS-NAMES\n')
-    fw.close()
+    f.close()
+    return asnames
--- pyasn/__init__.py
+++ pyasn/__init__.py
@@ -1,124 +1,84 @@
 # TODO: ADD COPYRIGHT + LICENSE / EXPLANATIONS WITH LINKS TO ORIG PROJECTS
+"""
+TODO: The docstring should list the pyasn() classes, with a one-line summary of it.
+ also:   - explain what module is used for, e.g. historical, fast local lookups
+         - how to initliaize and lookup - or the object that needs to be called
+"""
+
 from .pyasn_radix import Radix
-import pickle
-import zlib
-import base64
 
-#"""TODO: The docstring for a module should generally list the classes, exceptions and functions 
-#(and any other objects) that are exported by the module, with a one-line summary of each. """
-# also:   - explain what module is used for, e.g. historical, fast local lookups
-#         - how to initliaize and lookup - or the object that needs to be called
-#         - and where to download the IPASNDB files (or how to convert them from the BGP MRT dumps) - or the point in documentation          
-
-
-#__version__ = '1.5' ?
-#__all__ = ['pyasn'] ?
-
-        
-  
 class pyasn(object):  
-    """TODO: The docstring for a class should summarize its behavior and list the public methods and instance variables."""
-        
-    def __init__(self, ipasndb, binary=False, skip_names=True): 
-        """pyasn.pyasn(ipasndb, binary=False, skip_names=True)
-\nCreates a new instance of pyasn       
-ipasndb    = Filename of the IP-ASN-database to load
-binary     = set to True if ipasndb is binary format (faster but only IPv4 support) 
-skip_names = set to False to load autonomous system names if present in db (slower)
-\nThe database can be a simple text file with lines of "NETWORK/BITS\ASN"
-It can also be binary for faster loading, and contain AS-Names.
-You can create the database files using pyasn-helper scripts from BGP-MRT-dumps.
-Or download pre-made IPASNDB files from pyasn homepage."""          
+    """
+    TODO: The docstring for a class should summarize its behavior and list the public methods and instance variables.
+    """
+
+    def __init__(self, ip_asn_db_file, as_names_file=None, binary=False):
+        """
+        Creates a new instance of pyasn\n
+        ip_asn_db_file
+            Filename of the IP-ASN-database to load
+            The database can be a simple text file with lines of "NETWORK/BITS\ASN"
+            You can create the database files using pyasn-helper scripts from BGP-MRT-dumps.
+            Or download prebuilt database files from pyasn homepage.
+        as_names_file
+            if given, loads autonomous system names from this file (slower)
+        binary
+            set to True if ip_to_asn_file is in binary format (faster but only IPv4 support)
+        """
         self.radix = Radix()
-        # note: this class uses functionality provided by the underlying RADIX class (implemented in C for speed); 
-        #       actions such as add and delete nodes can be run on the underlying radix tree if required; that's why we expose "radix"
-        self._dbfilename = ipasndb
+        # we uses functionality provided by the underlying RADIX class (implemented in C for speed);
+        # actions such as add and delete nodes can be run on the radix tree if required - that's why its exposed
+        self._ipasndb_file = ip_asn_db_file
+        self._asnames_file = as_names_file
         self._binary = binary
-        self._records = self.radix.load_ipasndb(ipasndb, binary=binary)
-        self._asnames = self._read_asnames() if not skip_names else None
-    #
-    
+        self._records = self.radix.load_ipasndb(ip_asn_db_file, binary=binary)
+        self._asnames = self._read_asnames() if as_names_file else None
+
+
     def _read_asnames(self):
         """read autonomous system names, if present from both the text and  binary db formats"""
-        asnames = None 
-        f = open(self.ipasndb, "rb" if self.binary else "rt")  # codec, if required for text, should be ASCII/LATIN
-        if not self.binary:        
-            # in the text file, asnames is stored as the dumped version of asnames dictionary; it is compressed for efficency, and stored in BASE64
-            # to maintain compatibility with older pyasn, the lines are prepended with (;), and the whole section starts/ends with asnames' so load_ipasndb can skip it
-            # TODO: complete and test. including if strings have unicode how it will read in py2 ?  and that base64 returns BINARY in python 3; what to do with that?
-            section_names = False
-            blob = [] 
-            for s in f:
-                if section_names and s.startswith('; END-AS-NAMES'):
-                    break       
-                elif section_names:
-                    assert s[0]==';'
-                    blob.append(s[1:-1])  # strip start and end
-                elif s.startswith('; START-AS-NAMES'):
-                    # perhaps for  speed, a seek at the start of the file to this section can be done, if pos saved in comment. but might be os-dep and fragile 
-                    section_names = True
-            if blob:
-                blob = base64.b64decode(blob)  
-                blob = zlib.decompress(blob)  
-                asnames = pickle.loads(blob)  # use pickle over marshal; marshal might not be compatible across python versions. 
-        else:
-            # for binary files, at start of file, have a structure w/ *whether contains asnames* and *# records*. then we skip # of recordsx9 bytes, and go to the blob;
-            # read it, unzip it, demarhasl / depickle it to asnames
-            pass
-        f.close()
-        return asnames
-            
-        
-    def lookup_asn(self, ip):
-        """pyasn.lookup_asn(ip)\n
-Returns the Autonomous System Number that has holds this IP address (as advertised on the BGP tables).
-Returns None if the IP address is not found (meaning that it was not advertised on BGP, and is globlaly unrouted).
-Raises ValueError if passed an invalid IP address. """
-        rn = self.radix.search_best(ip)
-        return rn.asn  if rn else None
+        raise Exception("Not implemented")
+        # todo: test a variety of formats for fastest performance in loading & disc size
+        #           - a text file with ASN & AS-NAMES
+        #           - gzip of above
+        #           - "anydbm"
+        #           -  even pickle & compress
+        # todo: how should the as-names file be stored for binary format? (one implementation already in pyhelper for this)
 
-    def lookup_asn_prefix(self, ip):
-        """pyasn.lookup_asn_prefix(ip)\n
-Returns the Autonomous System Number that has holds this IP address, as well as the actual prefix it was advertised as part of.
-Returns None,None if the IP address is not found (meaning that it was not advertised on BGP, and is globlaly unrouted).
-Raises ValueError if passed an invalid IP address. 
-Supports IPv4 & IPv6"""
-        rn = self.radix.search_best(ip)
-        return (rn.asn, rn.prefix)  if rn else (None, None)
-              
-    # convenience functions.  todo: add docstrings
-    @property
-    def all_prefixes(self): 
-        return self.radix.prefixes() 
 
-    @property        
-    def all_asnames(self): 
-        return self._asnames.values() if self._asnames else []
-        
-    @property        
-    def all_asns(self): 
-        # todo: can cache for speed
-        return set([self.lookup_asn(px.split('/')[0]) for px in self.radix.prefixes()])
-        
+    def lookup(self, ip_address):
+        """
+        Returns (asn, prefix) of a given IP address.\n
+        'asn' is the Autonomous System Number that holds this IP address, as advertised on BGP.
+        'prefix' is the best matching prefix in the BGP table for this IP address.\n
+        Returns (None, None) if the IP address is not found (=not advertised, unreachable)
+        Raises ValueError an invalid IP address is passed.
+        """
+        rn = self.radix.search_best(ip_address)
+        return (rn.asn, rn.prefix) if rn else (None, None)
+
+
     def get_as_prefixes(self, asn):
-        # todo: can build a complete dictionary of {asn: set(prefixes)} on first call, and cache it, for next calls to the method
-        return [px for px in self.radix.prefixes() if self.lookup_asn(px.split('/')[0])==asn]
-                         
+        """Returns all Prefixes advertised by this ASN"""
+        # note: can build a full dict of {asn: set(prefixes)} on first call, and cache it for subsequent calls to method
+        return [px for px in self.radix.prefixes() if self.lookup(px.split('/')[0])[0] == asn]
+
+
     def get_as_name(self, asn):
+        """Returns the AS-Name for this ASN"""
         if not self._asnames:
-            raise Exception("autonomous system names not loaded from file during initialization")  # todo: or none?
+            raise Exception("autonomous system names not loaded during initialization")  # todo: or none?
         return self._asnames.get(asn, "???")                  
-        
+
+
     def __repr__(self):
-        names = "has as-names" if self._asnames else "no as-names"
-        return "pyasn object. ipasndb: '%s' (%d prefixes; %s)" % (self._dbfilename, self._records, names)                
-    
-    
+        return "pyasn(ipasndb:'%s'; asnames:'%s') - %d prefixes" % (self._ipasndb_file, self._asnames_file, self._records)
+
     # todo: add two static converter methods to convert ASX.X format into 32bit ASN and vice-versa
 
-    
-   # PERSISTENCE support, use with pickle.dump(), pickle.load()
-   # todo: test  persistence support with pickle.dump. also need to persist/reload ASNAMES & the two _records & _dbfilename, if not done automatically
+
+    # Persistence support, for use with pickle.dump(), pickle.load()
+    # todo: test persistence support.  (also persist/reload _asnames and other members, if not done automatically)
     def __iter__(self):
         for elt in self.radix:
             yield elt
@@ -132,7 +92,4 @@ Supports IPv4 & IPv6"""
             node.asn = asn
             
     def __reduce__(self):
-        return (Radix, (), self.__getstate__())
-
-        
-        
+        return Radix, (), self.__getstate__()
