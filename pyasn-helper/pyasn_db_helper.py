6faf2db884039d6064d6c7cfa65809456da153f4
Arman <arman.noroozian.developer@gmail.com>
2014-05-27 18:36:49 +0200

    Updated ignore file and some formatting

--- pyasn-helper/pyasn_db_helper.py
+++ pyasn-helper/pyasn_db_helper.py
@@ -10,29 +10,29 @@ def convert_ipasndb_to_binary(filename):
     f = open(filename)
     fw = open(filename+'.bin', 'wb')
     # SHOW STRUCTURE ADD HEADER-VER to binary file to show ipasn version + date
-    fw.write(str.encode('PYASN')) # magic header
-    fw.write(b'\x01') # binary format version 1
-    fw.write(struct.pack('I', 0)) # number of records; will need to be updated at the end.
+    fw.write(str.encode('PYASN'))  # magic header
+    fw.write(b'\x01')  # binary format version 1
+    fw.write(struct.pack('I', 0))  # number of records; will need to be updated at the end.
     nbytes = 12
     
     # let's store comments at start of text file as is in the binary file, useful for debugging, etc
     initial_comments = ""    
-    for s in f :
-        if s[0]!='#' and s[0]!='\n' and s[0]!=';':
+    for s in f:
+        if s[0] != '#' and s[0] != '\n' and s[0] != ';':
             break
         initial_comments += s
-    initial_comments = initial_comments.encode('ASCII', errors='replace')[:499] + b'\0' #  convert to bytes, trim, null terminate
+    initial_comments = initial_comments.encode('ASCII', errors='replace')[:499] + b'\0'  #convert to bytes, trim, null terminate
     fw.write(struct.pack('h', len(initial_comments))) 
-    fw.write(inital_comments) 
+    fw.write(initial_comments)
     nbytes += len(initial_comments)
     
     # main loop - convert prefixes
     f.seek(0)
     nrecs = 0
-    for s in f :
+    for s in f:
         if s.startswith('; START-AS-NAMES'):
             break  # finished prefix part
-        if s[0]=='#' or s[0]=='\n' or s[0]==';':
+        if s[0] == '#' or s[0]=='\n' or s[0] == ';':
             continue 
         prefix, asn = s[:-1].split()     
         asn = int(asn)
@@ -50,22 +50,24 @@ def convert_ipasndb_to_binary(filename):
     for s in f:
         if s.startswith('; END-AS-NAMES'):
             break       
-        assert s[0]==';'
+        assert s[0] == ';'
         blob.append(s[1:-1]) 
     if blob:
         blob = base64.b64decode(blob)  
-        fw.write(struct.pack('I', len(blob))  # number of bytes
+        fw.write(struct.pack('I', len(blob)))  # number of bytes
         fw.write(blob)
     # almost done. update number of records at start of file.
     f.close()        
     fw.seek(6)
-    fw.write(struct.pack('I', nrecs)) 
+    fw.write(struct.pack('I', nrecs))
     fw.close()    
-    return (nbytes, nrecs)
+    return nbytes, nrecs
              
 
 def convert_asnamelist_to_b64blob(filein, fileout):
-    """convert an asn + as-name list (one per line, space after ASN, 32bits with dots) to the compressed-based64-asnames-dictionary (readable by pyasn)"""
+    """convert an asn + as-name list (one per line, space after ASN, 32bits with dots)
+    to the compressed-based64-asnames-dictionary (readable by pyasn)
+    """
     # todo: compare this with using ANYDB regarding sizes, etc
     # todo: perhaps it's better if the function returns a string, instead of writing directly to an out-file
     # todo: Test on py2 (re utf/bytes). 
@@ -83,13 +85,11 @@ def convert_asnamelist_to_b64blob(filein, fileout):
     z = zlib.compress(s) 
     zb = base64.b64encode(z)
 
-    fw.write(b'; START-AS-NAMES\n'):
     fw = open(fileout, 'wb')
+    fw.write(b'; START-AS-NAMES\n')
     for i in range(0, len(zb), 79):
         fw.write(b';')
         fw.write(zb[i:i+79])
         fw.write(b'\n')
-    fw.write(b'; END-AS-NAMES\n'):    
+    fw.write(b'; END-AS-NAMES\n')
     fw.close()
-    
-    
