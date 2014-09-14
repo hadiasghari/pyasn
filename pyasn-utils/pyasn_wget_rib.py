76d6f59b6cd2f3c0c339405dde77f14bef7c1bec
Hadi Asghari <hadiasghari@users.noreply.github.com>
2014-09-14 17:22:58 +0200

    minor import changes

--- pyasn-utils/pyasn_wget_rib.py
+++ pyasn-utils/pyasn_wget_rib.py
@@ -25,31 +25,31 @@
 
 
 from __future__ import print_function, division
-import urllib2  #TODO:
-import datetime
+import urllib2  #TODO:  python 3 equivalant
+from datetime import timedelta, date
 import subprocess
-import sys
+from sys import argv, exit, stdout
 
 
-if len(sys.argv) != 2:
-    print('usage: %s FILEWITHDATES' % (sys.argv[0]))
-    sys.exit()
+if len(argv) != 2:
+    print('usage: %s FILEWITHDATES' % (argv[0]))
+    exit()
 
-f = open(sys.argv[1])
+f = open(argv[1])
 if not f:
-    print("can't open %s" % sys.argv[1])
-    sys.exit()
+    print("can't open %s" % argv[1])
+    exit()
 
 for s in f:
     if not s.strip(): 
         break
-    if s[0]=='#': 
+    if s[0] == '#':
         continue
-    dt = datetime.date(int(s[:4]), int(s[4:6]), int(s[6:8]) )
+    dt = date(int(s[:4]), int(s[4:6]), int(s[6:8]) )
     
     url_dir = 'http://archive.routeviews.org/bgpdata/%d.%02d/RIBS/' % (dt.year, dt.month)
     print('searching %s ...' % url_dir)
-    sys.stdout.flush()
+    stdout.flush()
 
     http = urllib2.urlopen(url_dir)
     html = http.read()
@@ -62,7 +62,7 @@ for s in f:
         #assert ix != -1
         if ix == -1:
             print(str(dt) + '\tERROR - NOT FOUND')
-            dt += datetime.timedelta(days=1)
+            dt += timedelta(1)
             continue
 
     fname = html[ix:ix+21]
@@ -74,14 +74,11 @@ for s in f:
     assert ix != -1
     size = s[:ix]
     
-
     url_full = url_dir + fname
-    ret = subprocess.call(['wget', '-q', url_full]) #quiet mode
+    ret = subprocess.call(['wget', '-q', url_full])  # quiet mode
     ret = "" if ret == 0 else "[FAIL:%d]" % ret
 
     print('%s\t%s\t%s\t%s' % (dt, size, url_full, ret))
-    sys.stdout.flush()
-
-    dt += datetime.timedelta(days=1)
+    stdout.flush()
+    dt += timedelta(1)
 #
-
